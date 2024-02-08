33aa942d7d81c22873acf932da34937ba025e937
Sarel Baruch <sarelb25@gmail.com>
2024-02-08 17:09:32 +0200

    initial commit

---
+++ app/src/main/java/com/example/firsthitmonday/MyData.java
@@ -0,0 +1,16 @@
+package com.example.firsthitmonday;
+
+
+
+public class MyData {
+
+    static String[] nameArray = {"Shemesh", "Etti", "Sassi", "Tenzer", "Ogen", "Hemi", "Bar","Rakefet", "Marcus", "Sivan"};
+    static String[] description = {"Restaurant owner", "Shemesh's friend", "Chef", "Pastrama consumer", "Sassi's wife",
+                                    "Ogen's husband", "Live above the restaurant", "Bar's mother", "The new owner",
+                                    "Almost Shemesh's wife"};
+
+    static Integer[] drawableArray = {R.drawable.shemesh, R.drawable.etti, R.drawable.sasi,
+            R.drawable.tenzer, R.drawable.ogen, R.drawable.hemi, R.drawable.bar,
+            R.drawable.rakefet, R.drawable.marcus, R.drawable.sivan};
+
+}
